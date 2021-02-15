fa8c40286d33ac1a5e0d905d2d0ecb64035a8618
yuesong-feng <yuesong-feng@foxmail.com>
2021-02-15 20:06:54 +0800

    update knn

--- KNeighborsClassifier.py
+++ KNeighborsClassifier.py
@@ -1,4 +1,5 @@
 import numpy as np
+import collections
 
 class KNeighborsClassifier:
     def __init__(self):
@@ -6,28 +7,37 @@ class KNeighborsClassifier:
         self.points = []
 
     def fit(self, X, y):
+        # 保存所有点的信息
         for i in range(len(X)):
             self.points.append([X[i], y[i]])
         return
 
     def predict(self, X):
-        neighbors = []
+        classification = []
+        # 计算要预测的点到数据集中每一个点的距离
         for i in range(len(X)):
             distance = []
             for j in range(len(self.points)):
                 distance.append(sum((X[i] - self.points[j][0]) ** 2))
+            # distance是当前点到数据集中每一个点的距离
             tmp = self.points
             for k in range(len(tmp)):
                 tmp[k].append(distance[k])
             tmp = sorted(tmp, key=lambda x:x[2])
-            print(tmp[0:self.n_neighbors])
-        return
+            # tmp是一个有三项的数组，第一项是numpy数据点，第二项是类别，第三项是距离，按照距离从小到大排序
+            # 接下来计算距离最近前n个点的概率最大的类别
+            neighbors = []
+            for i in range(self.n_neighbors):
+                neighbors.append(tmp[i][1])
+            neighbors = collections.Counter(neighbors)
+            classification.append(neighbors.most_common(1)[0][0])
+        return classification
 
 if __name__ == "__main__":
     X_train = np.array([[1, 1], [2, 2], [1, 1.5], [3, 2], [3, 4.5], [3.2, 4.3]])
     y_train = np.array([0, 0, 0, 1, 1, 1])
     knn = KNeighborsClassifier()
     knn.fit(X_train, y_train)
-    X_test = np.array([[1.5, 1.3], [2, 1.8], [3, 3]])
+    X_test = np.array([[1.5, 1.3], [2, 1.8], [4, 5]])
     y_predict = knn.predict(X_test)
-    # print(knn.points)
+    print(knn.points)

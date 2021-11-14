e566eb2c95179a8fd9d63009e94e8bc5fa902e58
yuesong-feng <yuesong-feng@foxmail.com>
2021-11-14 13:27:43 +0800

    update

--- LogisticRegressionGD.py
+++ LogisticRegressionGD.py
@@ -1,6 +1,6 @@
 import numpy as np
-class AdalineGD(object):
-    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+class LogisticRegressionGD(object):
+    def __init__(self, eta=0.05, n_iter=100, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
@@ -14,13 +14,15 @@ class AdalineGD(object):
             errors = (y - output)
             self.w_[1:] += self.eta * X.T.dot(errors)
             self.w_[0] += self.eta * errors.sum()
-            cost = (errors ** 2).sum() / 2
+            cost = -y.dot(np.log(output)) - (1 - y).dot(np.log(1 - output))
             self.cost_.append(cost)
         return self
     def net_input(self, X):
         return np.dot(X, self.w_[1:]) + self.w_[0]
-    def activation(self, X):
-        return X
+    def activation(self, z):
+        return 1 / (1 + np.exp(-np.clip(z, -250, 250)))
     def predict(self, X):
-        return np.where(self.activation(self.net_input(X)) >= 0, 1, -1)
+        return np.where(self.net_input(X) >= 0, 1, 0)
+        # equivalent to:
+        # return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
     
